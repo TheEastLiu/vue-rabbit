c0f57d02db929561be5a55dcc3a69950925ed123
TheEastLiu <liujd1118messi@163.com>
2025-09-25 17:22:49 +0800

    element-plus配置完成

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,7 @@
 
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <el-button type="success">Success</el-button>
 </template>
 
 <style scoped>
